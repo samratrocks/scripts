83f766bb4011356ef51cc5bb27faf7607a421ed2
samrat.rocks <samrat.shiwakoti@gmail.com>
2018-11-04 13:24:31 -0500

    add python flask deploy script

---
+++ flask-deploy.sh
@@ -0,0 +1,92 @@
+sudo apt-get install -y ufw
+sudo ufw allow ssh
+sudo ufw allow http
+sudo ufw allow 443/tcp
+sudo ufw --force enable
+sudo ufw status
+
+sudo apt-get -y update
+sudo apt-get -y install python3 python3-venv python3-dev
+sudo apt-get -y install mysql-server postfix supervisor nginx git
+
+
+#``` Change this to create a basic flask application
+git clone https://github.com/miguelgrinberg/microblog
+cd microblog
+git checkout v0.17
+#```
+
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+
+pip install gunicorn
+echo "export FLASK_APP=$APPNAME.py" >> ~/.profile
+
+gunicorn -b localhost:8000 -w 4 $APPNAME:app
+
+
+
+/etc/supervisor/conf.d/$APPNAME.conf: Supervisor configuration.
+[program:$APPNAME]
+command=/home/$USER/$APPNAME/venv/bin/gunicorn -b localhost:8000 -w 4 $APPNAME:app
+directory=/home/$USER/$APPNAME
+user=$USER
+autostart=true
+autorestart=true
+stopasgroup=true
+killasgroup=true
+
+
+sudo supervisorctl reload
+sudo rm /etc/nginx/sites-enabled/default
+
+
+/etc/nginx/sites-enabled/$APPNAME: Nginx configuration.
+server {
+    # listen on port 80 (http)
+    listen 80;
+    server_name _;
+    location / {
+        # redirect any requests to the same URL but on https
+        return 301 https://$host$request_uri;
+    }
+}
+server {
+    # listen on port 443 (https)
+    listen 443 ssl;
+    server_name _;
+
+    # location of the self-signed SSL certificate
+    ssl_certificate /home/$USER/$APPNAME/certs/cert.pem;
+    ssl_certificate_key /home/$USER/$APPNAME/certs/key.pem;
+
+    # write access and error logs to /var/log
+    access_log /var/log/$APPNAME_access.log;
+    error_log /var/log/$APPNAME_error.log;
+
+    location / {
+        # forward application requests to the gunicorn server
+        proxy_pass http://localhost:8000;
+        proxy_redirect off;
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+    }
+
+    location /static {
+        # handle static files directly, without forwarding to the application
+        alias /home/$USER/$APPNAME/app/static;
+        expires 30d;
+    }
+}
+
+sudo service nginx reload
+
+#``` Deploying application updates
+# git pull                              # download the new version
+# sudo supervisorctl stop $APPNAME     # stop the current server
+# flask db upgrade                      # upgrade the database
+# flask translate compile               # upgrade the translations
+# sudo supervisorctl start $APPNAME    # start a new server
+# ```
